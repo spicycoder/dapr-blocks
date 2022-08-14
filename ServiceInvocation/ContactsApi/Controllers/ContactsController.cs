f0c7c88dd07314d9ae01cbd96679093c3483d220
SpicyCoder <spk2naveen@hotmail.com>
2022-08-15 04:11:16 +0530

    Logs + Seq

--- ServiceInvocation/ContactsApi/Controllers/ContactsController.cs
+++ ServiceInvocation/ContactsApi/Controllers/ContactsController.cs
@@ -8,6 +8,13 @@ namespace ContactsApi.Controllers
     [ApiController]
     public class ContactsController : ControllerBase
     {
+        private readonly ILogger<ContactsController> _logger;
+
+        public ContactsController(ILogger<ContactsController> logger)
+        {
+            _logger = logger;
+        }
+
         [HttpGet("read")]
         public async Task<ActionResult<Contact>> Read(string name)
         {
@@ -15,6 +22,8 @@ namespace ContactsApi.Controllers
                 .RuleFor(x => x.Email, x => x.Internet.Email())
                 .RuleFor(x => x.Phone, x => x.Phone.PhoneNumber())
                 .Generate();
+
+            _logger.LogInformation("Contact information of {hero} is Email: {email}, Phone: {phone}", name, contact.Email, contact.Phone);
 
             return Ok(contact);
         }
